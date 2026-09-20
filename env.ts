fa89d5e89819c97835485d4fc1abe67532c2b3b8
Dariusz Filipiak <darekf77@gmail.com>
2026-09-20 17:12:28 +0200

    release: v21.0.235

--- env.ts
+++ env.ts
@@ -7,7 +7,7 @@ const env: Partial<EnvOptions> = {
     useDomain: true,
   },
   config: {
-    googleSecret: () => `$$$TAON_ENCRYPTED_START$$$v1:600000:U3NpnP07KY0c4LtZrejZsA==:LPv32FPgY0rNsBcx:+VATqikyAndyKkRL1tg5WgPbv/Vro2QyZqdslrlZMvAHJ8pkoXN1pVrQviABNFEVY9ug$$$TAON_ENCRYPTED_END$$$`,
+    googleSecret: () => `$$$TAON_ENCRYPTED_START$$$v1:600000:G+wKPKbNIqo2HfcCZ1F/UA==:muQFsICTOqwc7y/b:/jQups1I4RNW9Qf6jQH6GqnG8CQNmYbT8xm5Y+Fo8fUarHzNl+sz9KZjk/gXh9zjnIqz$$$TAON_ENCRYPTED_END$$$`,
     googleClientId: '289576612173-2mh31b67pmq2qdes5jedno9tr1m542es.apps.googleusercontent.com',
   },
   loading: {
